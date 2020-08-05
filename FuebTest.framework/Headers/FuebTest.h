2b4a4307a68d102743f6af9b4172f19861a0836b
yuyouliang <yuyouliang@dcorepay.com>
2020-08-05 21:22:45 +0800

    首次提交

---
+++ FuebTest.framework/Headers/FuebTest.h
@@ -0,0 +1,19 @@
+//
+//  FuebTest.h
+//  FuebTest
+//
+//  Created by 点芯在线 on 2020/8/5.
+//  Copyright © 2020 dxzx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FuebTest.
+FOUNDATION_EXPORT double FuebTestVersionNumber;
+
+//! Project version string for FuebTest.
+FOUNDATION_EXPORT const unsigned char FuebTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FuebTest/PublicHeader.h>
+#import "FuebTestFunc.h"
+
---
+++ FuebTest.framework/Headers/FuebTestFunc.h
@@ -0,0 +1,19 @@
+//
+//  FuebTestFunc.h
+//  FuebTest
+//
+//  Created by 点芯在线 on 2020/8/5.
+//  Copyright © 2020 dxzx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FuebTestFunc : NSObject
+-(void)gotoIseeWeb;
+@property(nonatomic, strong) UIViewController *mController;
+@end
+
+NS_ASSUME_NONNULL_END
